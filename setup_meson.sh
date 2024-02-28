af11b3556dcda8989ae64f5962e188911de3535b
haoyu <164346864@qq.com>
2024-02-28 22:02:52 +0800

    修复 setup_meson.sh 脚本错误

--- setup_meson.sh
+++ setup_meson.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-meson setup --wipe builddir -Denable-shared-library=true -Denable-example=true -Denable-fuzz=true
+meson setup --wipe builddir -Denable-example=true -Denable-fuzz=true
 
 # ninja -C builddir
